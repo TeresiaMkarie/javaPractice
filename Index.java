04de81eff8fe18a096071aaaa2d27921bef1d228
TeresiaMkarie <teresiajomo1@gmail.com>
2025-09-18 15:05:51 +0300

    mwenga

---
+++ Index.java
@@ -0,0 +1,27 @@
+public class Index{
+    public static void  main (String[]args){
+        int[]number={0,1,2,3,4,5};
+        int i=0;
+        while(i<number.length){
+            System.out.println(number[i]);
+            i++;
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
